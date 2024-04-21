59ad73141b95357ebe385560b80a72a983d0d842
Sarthak Sharma <sarthaksharma1105@gmail.com>
2024-04-21 14:02:57 +0530

    added requiremnts file

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ input_file = f"{in_dir}/24_jpg.rf.ddd99848038b1a40f63a854139acc8d3.jpg"
 name = Path(input_file).stem
 output_file = f'{out_dir}/{name}_pred.jpg'
 
-# Load the YOLO model
+# Load the YOLO modell
 model = YOLO('/home/satyukt/Sarthak/oil_well_saturn/Models/best.onnx')
 
 # Run inference on the input file
